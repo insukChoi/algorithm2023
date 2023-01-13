6f104f25b327dd6d0a71db2f97a1b28dc0822840
insuk <cis1725@gmail.com>
2023-01-13 21:56:15 +0900

    깊이/너비 우선 탐색(DFS/BFS)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # algorithm2023
-## https://school.programmers.co.kr/learn/challenges?tab=algorithm_practice_kit
-## https://leetcode.com/problemset/all/
+
+### 프로그래머스
+- https://school.programmers.co.kr/learn/challenges?tab=algorithm_practice_kit
+
+### LeetCode 
+- https://leetcode.com/problemset/all/
--- src/main/java/com/muzi/algorithm/programmers/Network.java
+++ src/main/java/com/muzi/algorithm/programmers/Network.java
@@ -5,8 +5,29 @@ package com.muzi.algorithm.programmers;
  * @see <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43162">네트워크</a>
  */
 public class Network {
+    private boolean[] visit = null;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                answer++;
+                DFS(i, computers, n);
+            }
+        }
+
         return answer;
+    }
+
+    private void DFS(final int i, final int[][] computers, final int n) {
+        visit[i] = true;
+
+        for (int k = 0; k < n; k++) {
+            if (!visit[k] && computers[i][k] == 1) {
+                DFS(k, computers, n);
+            }
+        }
     }
 }
