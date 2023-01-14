71821fea3add77d45c233ff6f2d727d6f87d440c
insuk <cis1725@gmail.com>
2023-01-14 20:35:24 +0900

    게임 맵 최단거리 준비

---
+++ src/main/java/com/muzi/algorithm/programmers/GameMapShortestDistance.java
@@ -0,0 +1,12 @@
+package com.muzi.algorithm.programmers;
+
+/**
+ * 깊이/너비 우선 탐색(DFS/BFS)
+ * @see [게임 맵 최단거리](https://school.programmers.co.kr/learn/courses/30/lessons/1844)
+ */
+public class GameMapShortestDistance {
+    public int solution(int[][] maps) {
+        int answer = -1;
+        return answer;
+    }
+}
