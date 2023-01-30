7a9157dc06af42f1215bf934c8994eeb9eccf23f
insuk <cis1725@gmail.com>
2023-01-30 22:18:28 +0900

    Find First and Last Position of Element in Sorted Array

---
+++ src/main/java/com/muzi/algorithm/leetcode/FindFirstLastPositionOfElement.java
@@ -0,0 +1,55 @@
+package com.muzi.algorithm.leetcode;
+
+/**
+ * 34. Find First and Last Position of Element in Sorted Array
+ * @see [find-first-and-last-position-of-element-in-sorted-array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)
+ */
+public class FindFirstLastPositionOfElement {
+    public int[] searchRange(int[] nums, int target) {
+        final int firstIndex = firstIndex(nums, target);
+        final int lastIndex = lastIndex(nums, target);
+        return new int[]{firstIndex, lastIndex};
+    }
+
+    private int firstIndex(final int[] nums, final int target) {
+        int returnIndex = -1;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target){
+                returnIndex = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return returnIndex;
+    }
+
+    private int lastIndex(final int[] nums, final int target) {
+        int returnIndex = -1;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target){
+                returnIndex = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return returnIndex;
+    }
+}
---
+++ src/test/java/com/muzi/algorithm/leetcode/FindFirstLastPositionOfElementTest.java
@@ -0,0 +1,60 @@
+package com.muzi.algorithm.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindFirstLastPositionOfElementTest {
+    @Test
+    void test1() {
+        int[] input = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{3, 4});
+    }
+
+    @Test
+    void test2() {
+        int[] input = {5, 7, 7, 8, 8, 10};
+        int target = 6;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{-1, -1});
+    }
+
+    @Test
+    void test3() {
+        int[] input = {};
+        int target = 0;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{-1, -1});
+    }
+
+    @Test
+    void test4() {
+        int[] input = {1};
+        int target = 1;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{0, 0});
+    }
+
+    @Test
+    void test5() {
+        int[] input = {2,2};
+        int target = 2;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{0, 1});
+    }
+
+    @Test
+    void test6() {
+        int[] input = {1};
+        int target = 0;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{-1, -1});
+    }
+
+    @Test
+    void test7() {
+        int[] input = {1,4};
+        int target = 4;
+        assertThat(executeSolution(input, target)).isEqualTo(new int[]{1, 1});
+    }
+
+    private int[] executeSolution(int[] input, int target) {
+        return new FindFirstLastPositionOfElement().searchRange(input, target);
+    }
+}
