8c47c2083e6dde0bdd9240856837325ec3fa4a34
insuk <cis1725@gmail.com>
2023-04-04 21:27:11 +0900

    MakeBiggestNumber test code

--- src/test/java/com/muzi/algorithm/programmers/MakeBiggestNumberTest.java
+++ src/test/java/com/muzi/algorithm/programmers/MakeBiggestNumberTest.java
@@ -16,20 +16,19 @@ class MakeBiggestNumberTest {
 
     @Test
     void test2() {
-        String number = "1924";
-        int k = 2;
-        assertThat(executeSolution(number, k)).isEqualTo("94");
+        String number = "1231234";
+        int k = 3;
+        assertThat(executeSolution(number, k)).isEqualTo("3234");
     }
 
     @Test
     void test3() {
-        String number = "1924";
-        int k = 2;
-        assertThat(executeSolution(number, k)).isEqualTo("94");
+        String number = "4177252841";
+        int k = 4;
+        assertThat(executeSolution(number, k)).isEqualTo("775841");
     }
 
     private String executeSolution(String number, int k) {
         return solution.solution(number, k);
     }
-
 }
