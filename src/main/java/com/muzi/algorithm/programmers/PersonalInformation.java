95dd2bfef22c5a0f6bd9bf05583683f61732431b
insuk <cis1725@gmail.com>
2023-05-19 23:59:24 +0900

    개인정보 수집 유효기간

---
+++ src/main/java/com/muzi/algorithm/programmers/PersonalInformation.java
@@ -0,0 +1,70 @@
+package com.muzi.algorithm.programmers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * 2023 KAKAO BLIND RECRUITMENT > 개인정보 수집 유효기간
+ * @see <a href="https://school.programmers.co.kr/learn/courses/30/lessons/150370">개인정보 수집 유효기간</a>
+ */
+public class PersonalInformation {
+
+    private static final String EMPTY_STRING = " ";
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        final ArrayList<Integer> answerList = new ArrayList<>();
+
+        LocalDate localToday = convertLocalDateWithBasicIsoDate(today);
+        for (int i = 0; i < privacies.length; i++) {
+            final String privacy = privacies[i];
+            final String targetDate = privacy.split(EMPTY_STRING)[0];
+            final String targetCharacter = privacy.split(EMPTY_STRING)[1];
+
+            if (expiredDate(terms, targetDate, targetCharacter)
+                    .isBefore(localToday)) {
+                answerList.add(i+1);
+            }
+        }
+
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+    /**
+     * 주어진 파라미터에서 예상되는 만료 일자를 구한다.
+     *
+     * @param terms = ["A 6", "B 12", "C 3"]
+     * @param targetDate = "YYYY.MM.DD"
+     * @param targetCharacter = "A"
+     */
+    private LocalDate expiredDate(final String[] terms, final String targetDate, final String targetCharacter) {
+        return convertLocalDateWithBasicIsoDate(targetDate)
+                .plusMonths(getTermsMonth(terms, targetCharacter))
+                .minusDays(1);
+    }
+
+    /**
+     * terms = ["A 6", "B 12", "C 3"] 처럼 [targetCharacter, month] 형식에서
+     * targetCharacter 에 맞는 month 를 찾아 return 한다.
+     */
+    private long getTermsMonth(final String[] terms, final String targetCharacter) {
+        return Long.parseLong(Arrays.stream(terms)
+                .filter(it -> it.contains(targetCharacter))
+                .map(it -> it.split(EMPTY_STRING)[1])
+                .findFirst()
+                .get()
+        );
+    }
+
+    /**
+     * YYYY.MM.DD 형식의 String 을 LocalDate 로 변환한다.
+     *
+     * @param YYYY.MM.DD
+     */
+    private static LocalDate convertLocalDateWithBasicIsoDate(final String today) {
+        return LocalDate.parse(today.replaceAll("\\.", ""), DateTimeFormatter.BASIC_ISO_DATE);
+    }
+}
---
+++ src/test/java/com/muzi/algorithm/programmers/PersonalInformationTest.java
@@ -0,0 +1,29 @@
+package com.muzi.algorithm.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PersonalInformationTest {
+    private final PersonalInformation personalInformation = new PersonalInformation();
+
+    @Test
+    void test1() {
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+
+        assertThat(personalInformation.solution(today, terms, privacies))
+                .isEqualTo(new int[]{1, 3});
+    }
+
+    @Test
+    void test2() {
+        String today = "2020.01.01";
+        String[] terms = {"Z 3", "D 5"};
+        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+
+        assertThat(personalInformation.solution(today, terms, privacies))
+                .isEqualTo(new int[]{1, 4, 5});
+    }
+}
