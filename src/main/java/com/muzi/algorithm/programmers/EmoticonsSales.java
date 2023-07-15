db8fd25e8d86b105f39a82dbceae1b46152aa2eb
insuk <cis1725@gmail.com>
2023-07-15 22:07:05 +0900

    이모티콘 할인행사

---
+++ src/main/java/com/muzi/algorithm/programmers/EmoticonsSales.java
@@ -0,0 +1,48 @@
+package com.muzi.algorithm.programmers;
+
+public class EmoticonsSales {
+    private static final int[] DISCOUNT = {10, 20, 30, 40};
+    private int maxOfSubscribe;
+    private int maxOfCost;
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        findResult(0, emoticons.length, new int[emoticons.length], users, emoticons);
+        return new int[]{maxOfSubscribe, maxOfCost};
+    }
+
+    public void findResult(int index, int emoticonsLength, int[] discounts, int[][] users, int[] emoticons) {
+        if (index == emoticonsLength) {
+            int subscribe = 0;
+            int cost = 0;
+
+            for (int[] user : users) {
+                int userDiscountRate = user[0];
+                int userMaxCost = user[1];
+
+                int sum = 0;
+
+                for (int i = 0; i < emoticons.length; i++) {
+                    if (discounts[i] >= userDiscountRate) {
+                        sum += emoticons[i] / 100 * (100 - discounts[i]);
+                    }
+                }
+                if (sum >= userMaxCost) subscribe++;
+                else cost += sum;
+            }
+
+            if (subscribe > maxOfSubscribe) {
+                maxOfSubscribe = subscribe;
+                maxOfCost = cost;
+            } else if (subscribe == maxOfSubscribe) {
+                maxOfCost = Math.max(maxOfCost, cost);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            discounts[index] = DISCOUNT[i];
+            findResult(index + 1, emoticonsLength, discounts, users, emoticons);
+        }
+    }
+}
